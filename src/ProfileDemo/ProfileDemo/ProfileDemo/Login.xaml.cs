31f20ebba5e915a2c3c7c8491ee20caa20c3b3fd
mizanur rahaman <mizanurrahaman020195@gmail.com>
2022-08-12 08:32:49 +0600

    save

---
+++ src/ProfileDemo/ProfileDemo/ProfileDemo/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProfileDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (usernameText.Text == "admin" && passwordText.Text == "admin123")
+            {
+                Navigation.PushAsync(new Profile());
+            }
+            else
+            {
+                DisplayAlert("Warning", "Invalid UserName OR Password", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Register());
+        }
+    }
+}
